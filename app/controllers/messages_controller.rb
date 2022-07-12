930f75c8956c31319167637f8e8913ce81f81906
roast000 <107483506+roast000@users.noreply.github.com>
2022-07-12 18:23:07 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
   def create
     @room = Room.find(params[:room_id])
@@ -9,6 +10,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   
@@ -16,5 +18,6 @@ class MessagesController < ApplicationController
     def message_params
       params.require(:message).permit(:content).merge(user_id: current_user.id)
     end
-  end
+  end  
+end
   
